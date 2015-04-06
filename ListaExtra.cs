f7b6f2ab5b16f53f958e9a5e52c2e24b7a221e4a
karv <karvayoEdgar@gmail.com>
2015-04-05 19:37:17 -0500

    Agrega clase Set
    Conjunto sin control sobre el orden.

--- ListaExtra.cs
+++ ListaExtra.cs
@@ -409,3 +409,43 @@ namespace ListasExtra
 		}
 	}
 }
+
+namespace ListaExtra.Set
+{
+	/// <summary>
+	/// Representa un conjunto de elementos sin un control sobre el orden.
+	/// </summary>
+	/// <typeparam name="T">Tipo de objetos</typeparam>
+	public class Set<T> : List<T>
+	{
+		Random r = new Random();
+		/// <summary>
+		/// Agrega un objeto al conjunto.
+		/// </summary>
+		/// <param name="x"></param>
+		public new void Add(T x)
+		{
+			base.Insert(r.Next(Count + 1), x);
+		}
+
+		/// <summary>
+		/// Regresa un arreglo con los objetos en el conjunto, sin repetición.
+		/// </summary>
+		/// <returns></returns>
+		public T[] ToArray()
+		{
+			return base.ToArray();
+		}
+
+		/// <summary>
+		/// Devuelve un elemento de este conjunto.
+		/// </summary>
+		public T Next
+		{
+			get
+			{
+				return base[r.Next(Count + 1)];
+			}
+		}
+	}
+}
