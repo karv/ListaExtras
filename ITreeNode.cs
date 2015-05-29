9c00cf301c0852ff488c996ed8edf3fab76d4c6a
Edgar Carballo <karvayoedgar@gmail.com>
2015-05-28 19:38:05 -0500

    Apropiado IEnumerable es ahora TreeNode

---
+++ ITreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasExtra.Treelike
+{
+	public interface ITreeNode<T> : IEnumerable<T[]>
+	{
+		System.Collections.Generic.List<ITreeNode<T>> getSucc { get; }
+
+		ITreeNode<T> getTreeFrom (T[] stem);
+
+		T[] objeto { get; }
+	}
+
+	public static class ExtITreeNode
+	{
+		public static ITreeNode<T> FindSucc<T> (this ITreeNode<T> tree, T obj)
+		{
+			foreach (var x in tree.getSucc) {
+				if (x.objeto.Equals (obj))
+					return x;
+			}
+			throw new Exception (string.Format ("No se encuentra el sucesor {0} en el árbol {1}.", obj, tree));
+		}
+
+		/// <summary>
+		/// Revisa si este árbol tiene sucesores.
+		/// </summary>
+		/// <returns><c>true</c>, si el árbol no tiene sucesores, <c>false</c> otherwise.</returns>
+		/// <param name="tree">Tree.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static bool esNodoMuerto<T> (this ITreeNode<T> tree)
+		{
+			return (tree.getSucc == null || tree.getSucc.Count == 0);
+		}
+	}
+}
---
+++ TreeLista.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasExtra.Treelike
+{
+	public class TreeLista<T> : TreeNode<T>, IEnumerable<T[]>
+	{
+		public enum enumOpcionOrden
+		{
+			PadrePrimero,
+			HijosPrimero
+		}
+
+		public TreeLista () : base (null)
+		{
+		}
+
+		public void Add (T[] item)
+		{
+			getSucc.Add (new TreeNode<T> (item));
+		}
+
+		public void Add (T item)
+		{			
+			T[] Adding = new T[objeto.Length + 1];
+			objeto.CopyTo (Adding, 0);
+			Adding [objeto.Length] = item;
+			Add (Adding); 
+		}
+
+		public void Add (ITreeNode<T> item)
+		{
+			getSucc.Add (item);
+		}
+
+		public void Clear ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool Contains (T[] item)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void CopyTo (T[][] array, int arrayIndex)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool Remove (T[] item)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public int Count {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public bool IsReadOnly {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+		/*
+		IEnumerator<T> IEnumerable<T[]>.GetEnumerator ()
+		{
+			return new Treelike.TreeEnumerator<T> (getSucc, objeto);
+		}
+*/
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator ()
+		{
+			return new Treelike.TreeEnumerator<T> (getSucc, objeto);
+		}
+
+		System.Collections.Generic.IEnumerator<T[]> System.Collections.Generic.IEnumerable<T[]>.GetEnumerator ()
+		{
+			return new Treelike.TreeEnumerator<T> (getSucc, objeto);
+		}
+
+	}
+}
+
