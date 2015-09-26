b0f0ad6f73d1a79e7166c46b5b842a5777706f8a
Edgar Carballo <karvayoEdgar@gmail.com>
2015-09-26 17:33:52 -0500

    Separar clases en un archivo cada uno

---
+++ ListaContador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ListasExtra
+{
+	/// <summary>
+	/// Es sólo una listaPeso de enteros largos.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class ListaContador<T> : ListaPeso<T, long>
+	{
+		public ListaContador ()
+			: base ((x, y) => x + y, 0)
+		{
+		}
+
+		public long CountIf (Func<T, bool> Selector)
+		{
+			long ret = 0;
+			foreach (var x in Keys) {
+				if (Selector.Invoke (x)) {
+					ret += this [x];
+				}
+			}
+			return ret;
+
+		}
+	}
+}
+
---
+++ ObjetoAcotado.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.Serialization;
+using System;
+
+namespace ListasExtra
+{
+	[DataContract]
+	public class ObjetoAcotado<T>
+	{
+		public T CotaSup;
+		public T CotaInf;
+		T _Valor;
+
+		public Func<T, T, Boolean> EsMenor { get; set; }
+
+		public T Valor {
+			get {
+				return _Valor;
+			}
+			set {
+
+				_Valor = EsMenor (value, CotaSup) ? (EsMenor (value, CotaInf) ? CotaInf : value) : CotaSup;
+				if (_Valor.Equals (CotaInf)) {
+					EventHandler Handler = LlegóMínimo;
+					Handler (this, null);
+				}
+				if (_Valor.Equals (CotaSup)) {
+					EventHandler Handler = LlegóMáximo;
+					Handler (this, null);
+				}
+			}
+		}
+
+		public ObjetoAcotado (Func<T, T, Boolean> Comparador)
+		{
+			EsMenor = Comparador;
+		}
+
+		public ObjetoAcotado (Func<T, T, Boolean> Comparador, T Min, T Max, T Inicial)
+			: this (Comparador)
+		{
+			CotaInf = Min;
+			CotaSup = Max;
+			Valor = Inicial;
+		}
+
+		public override string ToString ()
+		{
+			return Valor.ToString ();
+		}
+		//Eventos
+		public event EventHandler LlegóMínimo;
+		public event EventHandler LlegóMáximo;
+	}
+}
+
