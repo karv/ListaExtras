2c6c880f4c7891b598cf430180b8d56c8d02dc1f
Edgar Carballo <karvayoEdgar@gmail.com>
2015-09-26 15:58:33 -0500

    Agregar prueba para ListaPeso

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,46 +1,59 @@
 ﻿using System;
+using ListasExtra;
+using System.Diagnostics;
 
 
 namespace Test
 {
 	class MainClass
 	{
-		static void TestSet ()
+		static void TestSet()
 		{
 			//ListasExtra.Set.Set<int> mySet = new ListasExtra.Set.Set<int> ();
 		}
 
-		static void TestLock ()
+		static void TestLock()
 		{
-			ListasExtra.Lock.ListaPesoBloqueable<int, int> bv = new ListasExtra.Lock.ListaPesoBloqueable<int, int> (((x, y) => x + y), 0);
+			ListasExtra.Lock.ListaPesoBloqueable<int, int> bv = new ListasExtra.Lock.ListaPesoBloqueable<int, int>(((x, y) => x + y), 0);
 		
-			bv [3] = 1;
-			bv [4] = 1;			
-			foreach (var x in bv) {
-				bv.Add (5, 1);
+			bv[3] = 1;
+			bv[4] = 1;
+			foreach (var x in bv)
+			{
+				bv.Add(5, 1);
 			}
 
-			System.Console.WriteLine ("");
+			System.Console.WriteLine("");
 
 			//ListasExtra.Lock.LockEnumerator
 		}
 
-		public static void TestTree ()
+		static void TestListaPeso()
 		{
-			ListasExtra.Treelike.TreeList<int> tl = new ListasExtra.Treelike.TreeList<int> ();
-			int[] o = { 1, 2, 3 };
-			Console.WriteLine (tl.Count);
-			tl.Add (o);
-			Console.WriteLine (tl.Contains (o));
-			Console.WriteLine (tl.Count);
-			tl.Remove (o);
-			Console.WriteLine (tl.Contains (o));
-			Console.WriteLine (tl.Count);
+			var lp = new ListaPeso<int>();
+			for (int i = 0; i < 100; i++)
+			{
+				lp[i] = 99 - i;
+			}
+			Debug.WriteLine(lp);
 		}
 
-		public static void Main (string[] args)
+		public static void TestTree()
 		{
-			TestLock ();
+			ListasExtra.Treelike.TreeList<int> tl = new ListasExtra.Treelike.TreeList<int>();
+			int[] o = { 1, 2, 3 };
+			Console.WriteLine(tl.Count);
+			tl.Add(o);
+			Console.WriteLine(tl.Contains(o));
+			Console.WriteLine(tl.Count);
+			tl.Remove(o);
+			Console.WriteLine(tl.Contains(o));
+			Console.WriteLine(tl.Count);
+		}
+
+		public static void Main(string[] args)
+		{
+			TestListaPeso();
 		}
 	}
 }
