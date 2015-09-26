ef9c98c0287c8d0d852fa52aa0a970a108e9550f
Edgar Carballo <karvayoEdgar@gmail.com>
2015-09-26 18:11:48 -0500

    Agregar proyecto de pruebas

---
+++ Pruebas/TestListaPeso.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using ListasExtra;
+using System;
+
+namespace Pruebas
+{
+	[TestFixture]
+	public class TestListaPeso
+	{
+		[Test]
+		public void TestCase()
+		{
+			var lp = new ListaPeso<int>();
+			for (int i = 0; i < 100; i++)
+			{
+				lp[i] = 99 - i;
+			}
+			Console.WriteLine(lp);
+			lp.AlCambiarValor += (sender, e) => 
+				Console.WriteLine(string.Format("lp[{0}] cambió de {1} a {2}", e.Key, e.Previo, e.Actual));
+			lp[1] = 98;
+
+		}
+	}
+}
+
---
+++ Pruebas/TreeList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ListasExtra.Treelike;
+using NUnit.Framework;
+
+namespace Pruebas
+{
+	[TestFixture]
+	public class TreeList
+	{
+		[Test]
+		public void Test()
+		{
+			var tl = new TreeList<int>();
+			int[] o = { 1, 2, 3 };
+			Console.WriteLine(tl.Count);
+			tl.Add(o);
+			Console.WriteLine(tl.Contains(o));
+			Console.WriteLine(tl.Count);
+			tl.Remove(o);
+			Console.WriteLine(tl.Contains(o));
+			Console.WriteLine(tl.Count);
+		}
+	}
+}
+
