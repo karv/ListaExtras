b4b97ea563ef0bbab4d9b8a9196125c71b652dfa
Edgar Carballo <karvayoEdgar@gmail.com>
2015-09-29 18:00:29 -0500

    Agregar métodos en IList y ICollection para obtener un elemento aleatorio ListasExtra.Extensiones.ColecciónExt

---
+++ CollecciónExt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasExtra.Extensiones
+{
+	public static class CollecciónExt
+	{
+		static readonly Random random = new Random ();
+
+		/// <summary>
+		/// Devuelve un elemento aleatorio de una lista.
+		/// </summary>
+		/// <param name="lista">Lista.</param>
+		/// <param name="r">The red component.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T Aleatorio<T> (this IList<T> lista, Random r = null)
+		{
+			if (lista == null)
+				throw new NullReferenceException ();
+			if (lista.Count == 0)
+				throw new IndexOutOfRangeException ("No se puede tomar un elemento de una lista vacía.");
+			
+			r = r ?? random;
+			return lista [r.Next (lista.Count)];
+		}
+
+		public static T Aleatorio<T> (this ICollection<T> coll, Random r = null)
+		{
+			if (coll == null)
+				throw new NullReferenceException ();
+			if (coll.Count == 0)
+				throw new IndexOutOfRangeException ("No se puede tomar un elemento de una colección vacía.");
+
+			r = r ?? random;
+			int currrnd = coll.Count;
+			foreach (var x in coll) {
+				if (r.Next (currrnd--) == 0) {
+					return x;
+				}
+			}
+			throw new Exception ("¡¡¡Ocurrió algo raro!!!");
+		}
+	}
+}
+
---
+++ Pruebas/IListExt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using ListasExtra.Extensiones;
+
+namespace Pruebas
+{
+	[TestFixture]
+	public class IListExt
+	{
+		[Test]
+		public void TestIListGetRandom ()
+		{
+			var list = new List<int> ();
+			for (int i = 0; i < 1000; i++) {
+				list.Add (i);
+			}
+			for (int j = 0; j < 1000; j++) {
+				int i = list.Aleatorio ();
+				Console.Write (i + " ");
+				Assert.True (list.Remove (i));
+			}
+			Assert.AreEqual (0, list.Count);
+		}
+
+		[Test]
+		public void TestCollectionRandom ()
+		{
+			var list = new List<int> ();
+			for (int i = 0; i < 1000; i++) {
+				list.Add (i);
+			}
+			for (int j = 0; j < 1000; j++) {
+				int i = list.Aleatorio ();
+				Console.Write (i + " ");
+				Assert.True (list.Remove (i));
+			}
+			Assert.AreEqual (0, list.Count);
+		}
+
+		[Test]
+		public void TestEmptyCollectionRandom ()
+		{
+			Assert.Throws<IndexOutOfRangeException> (
+				new TestDelegate (
+					delegate {
+						ICollection<int> g = new List<int> ();
+						g.Aleatorio ();
+					}
+				)
+			);
+		}
+	}
+}
