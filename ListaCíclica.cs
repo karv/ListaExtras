35c8eda4aaef0fff2132cfe1b1664b4c3419ba1e
Edgar Carballo <karvayoEdgar@gmail.com>
2015-10-15 18:25:08 -0500

    Agregar listacíclica
    Tiene Add, count this[] funcionando

---
+++ ListaCíclica.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace ListasExtra
+{
+	public class ListaCíclica<T>:List<T> //TEST
+	{
+		int _internalZero;
+
+		int InternalZero {
+			get {
+				return _internalZero;
+			}
+			set {
+				_internalZero = value % Count;
+			}
+		}
+
+		public ListaCíclica () : base ()
+		{
+		}
+
+		public ListaCíclica (IEnumerable<T> coll) : base (coll)
+		{
+		}
+
+		public new T this [int i] {
+			get {
+				return base [(i + InternalZero) % Count];
+			}
+			set {
+				base [(i + InternalZero) % Count] = value;
+			}
+		}
+
+		/// <summary>
+		/// Devuelve el siguiente elemento de la lista
+		/// </summary>
+		public T Siguiente {
+			get {
+				var ret = base [InternalZero];
+				InternalZero++;
+				return ret;
+			}
+		}
+
+		/// <Docs>The item to add to the current collection.</Docs>
+		/// <para>Adds an item to the current collection.</para>
+		/// <remarks>To be added.</remarks>
+		/// <exception cref="System.NotSupportedException">The current collection is read-only.</exception>
+		/// <summary>
+		/// Add the specified item.
+		/// </summary>
+		/// <param name="objeto">Objeto.</param>
+		public new void Add (T objeto)
+		{
+			Insert (InternalZero, objeto);
+			InternalZero++;
+		}
+	}
+}
---
+++ Pruebas/TestListaCíclica.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ListasExtra;
+
+namespace Pruebas
+{
+	[TestFixture]
+	public class TestListaCíclica
+	{
+		[Test]
+		public void Probar()
+		{
+			var testList = new ListaCíclica<int>();
+			for (int i = 0; i < 10; i++)
+			{
+				testList.Add(i);
+			}
+			Assert.AreEqual(0, testList.Siguiente);
+			for (int i = 10; i < 20; i++)
+			{
+				testList.Add(i);
+			}
+			Assert.AreEqual(1, testList.Siguiente);
+			Assert.AreEqual(20, testList.Count);
+
+
+		}
+	}
+}
+
