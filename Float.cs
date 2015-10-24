659eddf1e371d12aa04d662415b29fe300a52920
Edgar Carballo <karvayoEdgar@gmail.com>
2015-10-24 17:21:30 -0500

    Agregar Float acotado

---
+++ Float.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ListasExtra.Acotados
+{
+	public struct Float
+	{
+		float _valor;
+
+		public float Valor {
+			get {
+				return _valor;
+			}
+			set {
+				_valor = Math.Min (Math.Max (Min, value), Max);
+			}
+		}
+
+		public float Max { get; }
+
+		public float Min { get; }
+
+		public Float (float v, float min = float.NegativeInfinity, float max = float.PositiveInfinity)
+		{
+			Min = min;
+			Max = max;
+			_valor = v;
+		}
+
+
+		public Float () : this (0)
+		{
+		}
+
+		public static implicit operator float (Float val)
+		{
+			return val.Valor;
+		}
+	}
+}
