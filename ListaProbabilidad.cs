919bdd168566503ed00f4da95d68db7a4e450b3d
Edgar Carballo <karvayoEdgar@gmail.com>
2016-03-13 13:42:11 -0600

    Agregar lista de probabilidad

---
+++ ListaProbabilidad.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasExtra
+{
+	/// <summary>
+	/// Una lista de probabilidades
+	/// </summary>
+	public class ListaProbabilidad<T>
+	{
+		public ListaProbabilidad (Func<T, double> peso)
+		{
+			Randomizer = new Random ();
+			Peso = peso;
+		}
+
+		public ListaProbabilidad (Func<T, double> peso, Random rand)
+		{
+			Randomizer = rand;
+			Peso = peso;
+		}
+
+		/// <summary>
+		/// El generador de pseudoaleatorios.
+		/// </summary>
+		public Random Randomizer { get; }
+
+		/// <summary>
+		/// Función de peso
+		/// </summary>
+		public Func<T, double> Peso { get; }
+
+		readonly Dictionary<T, double> _data = new Dictionary<T, double> ();
+
+		/// <summary>
+		/// Agrega un objeto con peso
+		/// </summary>
+		public void Add (T obj)
+		{
+			_data.Add (obj, Peso (obj));
+		}
+
+		/// <summary>
+		/// Elimina un objeto
+		/// </summary>
+		public void Remove (T obj)
+		{
+			_data.Remove (obj);
+		}
+
+		/// <summary>
+		/// Selecciona aleatoriamente un objeto
+		/// </summary>
+		public T Select ()
+		{
+			if (Count == 0)
+				throw new NotMeasureException ();
+
+			var norm = _normalizedData ();
+			var st = Randomizer.NextDouble ();
+			foreach (var d in norm) {
+				st -= d.Value;
+				if (st <= 0)
+					return d.Key;
+			}
+			throw new Exception ("¿Qué pasó aquí?");
+		}
+
+		public int Count {
+			get {
+				return _data.Count;
+			}
+		}
+
+		public double Suma ()
+		{
+			var _dataSnapshot = new Dictionary<T, double> (_data);
+			double suma = 0;
+			foreach (var x in _dataSnapshot) {
+				suma += x.Value;
+			}
+
+			return suma;
+		}
+
+		Dictionary<T, double> _normalizedData ()
+		{
+			var suma = Suma ();
+			if (suma == 0)
+				throw new NotMeasureException ();
+			var _dataSnapshot = new Dictionary<T, double> (_data);
+			var ret = new Dictionary<T, double> ();
+			foreach (var x in _dataSnapshot) {
+				ret.Add (x.Key, x.Value / suma);
+			}
+
+			return ret;
+		}
+
+
+		[Serializable]
+		/// <summary>
+		/// Exception cuando se intenta usar una medida como probabilidad cuando no lo es.
+		/// </summary>
+		public class NotMeasureException : Exception
+		{
+			public NotMeasureException ()
+			{
+			}
+
+			public NotMeasureException (string message)
+				: base (message)
+			{
+			}
+
+			public NotMeasureException (string message, Exception inner)
+				: base (message, inner)
+			{
+			}
+
+			protected NotMeasureException (System.Runtime.Serialization.SerializationInfo info,
+			                               System.Runtime.Serialization.StreamingContext context)
+				: base (info,
+				        context)
+			{
+			}
+		}
+	}
+}
---
+++ Pruebas/TestListaProb.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using ListasExtra;
+using System.Runtime.Remoting.Metadata;
+using System.Collections.Generic;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Pruebas
+{
+	[TestFixture]
+	public class TestListaProb
+	{
+		[Test]
+		public void Test()
+		{
+			var prob = new ListaProbabilidad<int>(z => z);
+			var res = new Dictionary<int, double>(10);
+			for (int i = 0; i < 10; i++)
+			{
+				prob.Add(i);
+				res.Add(i, 0);
+			}
+			const int numPruebas = 100000;
+			for (int i = 0; i < numPruebas; i++)
+			{
+				var ind = prob.Select();
+				res[ind]++;
+			}
+
+			Assert.AreEqual(10, prob.Count);
+			Assert.AreEqual(45, prob.Suma());
+
+			const double expFactor = numPruebas / 45.0;
+
+			// Imprimir resultados
+			for (int i = 0; i < 10; i++)
+			{
+				var expect = expFactor * i;
+				var errAbs = expect - res[i];
+				var errRel = errAbs / expect;
+				if (i > 0)
+					Assert.True(Math.Abs(errRel) < 0.1);
+				Console.WriteLine("{0}\t\tE = {1}:\t\t{2}", res[i], expect, errRel);
+			}
+		}
+	}
+}
