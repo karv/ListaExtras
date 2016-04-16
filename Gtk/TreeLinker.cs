0e1975427bfa7699daec2cce2d97575c7dfe527c
Edgar Carballo <karvayoEdgar@gmail.com>
2016-04-16 14:32:58 -0500

    + TreeLinker

---
+++ Gtk/TreeLinker.cs
@@ -0,0 +1,33 @@
+﻿using ListasExtra.Árboles;
+using Gtk;
+using System;
+
+namespace ListasExtra.Gtk
+{
+	public static class TreeLinker
+	{
+		/// <summary>
+		/// Agrega una entrada a un TreeStore dado un IÁrbolEnraizado.
+		/// </summary>
+		/// <param name="store">Modelo de gtk.árbol donde copiar contenido</param>
+		/// <param name="tree">Árbol a copiar contenido</param>
+		/// <param name="encaje">Cómo se encaja el nodo del árbol en Store</param>
+		/// <param name="iter">Iterador dentro del gtk.árbol</param>
+		/// <typeparam name="T">Clase de nodos del árbol</typeparam>
+		public static void Append<T> (this TreeStore store,
+		                              IÁrbolEnraizado<T> tree,
+		                              Func<T, Array> encaje,
+		                              TreeIter? iter = null)
+		{
+			TreeIter newIter;
+			newIter = !iter.HasValue ? store.AppendValues (encaje (tree.Nodo)) : store.AppendValues (
+				iter.Value,
+				encaje (tree.Nodo));
+
+			foreach (var tr in tree.Sucesores)
+			{
+				Append<T> (store, tr, encaje, newIter);
+			}
+		}
+	}
+}
