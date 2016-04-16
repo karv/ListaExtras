e240beab1a97c3d50cdda199b2e3f49551b42fd1
Edgar Carballo <karvayoEdgar@gmail.com>
2016-04-16 14:04:56 -0500

    Agregar segundo proyecto para vínculos con gtk Agregar interface IÁrbolEnraizado

---
+++ Árboles/IÁrbolEnraizado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasExtra.Árboles
+{
+	/// <summary>
+	/// Representa una estructora de objetos con sucesores definidos.
+	/// </summary>
+	public interface IÁrbolEnraizado<T>
+	{
+		T Nodo { get; }
+
+		IEnumerable<IÁrbolEnraizado<T>> Sucesores { get; }
+	}
+}
